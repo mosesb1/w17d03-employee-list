5c8771421c955bcba801bf97626eeb3dbbc9db5d
Moses Burkhart <moses.burkhart1@gmail.com>
2022-04-06 14:57:53 -0400

    added placeholder text for search bar for clarity

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 export default function SearchBar({searchText, setSearchText}) {
     return <input onChange={(evt) => {
         setSearchText(evt.target.value)
-    }}type="text" className="search" />;
+    }}type="text" className="search" placeholder="Search by employee name"/>;
 }
   
